41b33c14ea780f54f06a167435cc6fcc3ed63889
Dipsy Wong <ycwongal@connect.ust.hk>
2022-09-10 16:49:16 +0800

    add logging

---
+++ src/server/logger.ts
@@ -0,0 +1,18 @@
+import winston from 'winston';
+import 'winston-daily-rotate-file';
+
+const transport = new winston.transports.DailyRotateFile({
+  filename: 'logs/application-%DATE%.log',
+  datePattern: 'YYYY-MM-DD-HH',
+  zippedArchive: true,
+  maxSize: '20m',
+  maxFiles: '7d',
+});
+
+const logger = winston.createLogger({
+  transports: [
+    transport,
+  ],
+});
+
+export default logger;
