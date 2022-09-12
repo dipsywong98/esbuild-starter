890ba2c0c2309e3579b96f13394a978e2c64d75d
Dipsy Wong <ycwongal@connect.ust.hk>
2022-09-12 23:51:22 +0800

    update build config

---
+++ ecosystem.config.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+
+const env = {
+  NODE_ENV: "production"
+}
+
+const pathToEnv = path.join(__dirname, '.env');
+if (fs.existsSync(pathToEnv)) {
+  const str = fs.readFileSync(pathToEnv, 'utf8');
+  str.split('\n').forEach((line) => {
+    const [key, ...value] = line.split('=');
+    if (key && value && value.length) {
+      env[key] = value.join('=');
+    }
+  });
+}
+
+module.exports = {
+  apps: [{
+    name: "esbuild-starter",
+    script: "./index.js",
+    env,
+    watch: true,
+    watch_delay: 1000,
+  }]
+}
