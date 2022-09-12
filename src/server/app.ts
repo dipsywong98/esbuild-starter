e70e63f563fde1d0e009c9ad27c34be75c3e6610
Dipsy Wong <ycwongal@connect.ust.hk>
2022-09-12 11:20:36 +0800

    add logger for http

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,9 +1,16 @@
 import express from 'express';
+import morgan from 'morgan';
 import config from '../common/config';
+import logger from './logger';
 
 const app = express();
 
 app.use('/', express.static('client'));
+app.use(morgan('tiny', {
+  stream: {
+    write: (message) => logger.http(message),
+  },
+}));
 app.get('/api/ping', (req, res) => {
   res.send(config.HELLO_WORLD);
 });
@@ -13,6 +20,12 @@ app.get('/api/releaseInfo', (req, res) => {
     version: config.APP_VERSION,
     hash: config.APP_HASH,
   });
+});
+
+app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
+  logger.error(`Error when handling ${req.path} ${err.stack ?? ''}`);
+  res.sendStatus(500);
+  next();
 });
 
 export default app;
--- src/server/logger.ts
+++ src/server/logger.ts
@@ -1,7 +1,15 @@
-import winston from 'winston';
+import winston, { format } from 'winston';
 import 'winston-daily-rotate-file';
 
-const transport = new winston.transports.DailyRotateFile({
+const { combine, timestamp, colorize } = format;
+
+declare module 'logform' {
+  interface TransformableInfo {
+    timestamp: string
+  }
+}
+
+const fileRotateTransport = new winston.transports.DailyRotateFile({
   filename: 'logs/application-%DATE%.log',
   datePattern: 'YYYY-MM-DD-HH',
   zippedArchive: true,
@@ -9,9 +17,18 @@ const transport = new winston.transports.DailyRotateFile({
   maxFiles: '7d',
 });
 
+const logFormat = winston.format.printf((info) => `${info.level} [${info.timestamp}]: ${JSON.stringify(info.message, null, 4)}`);
+
 const logger = winston.createLogger({
+  level: 'http',
+  format: combine(timestamp({
+    format: 'YYYY-MM-DDTHH:mm:ssZ',
+  }), logFormat),
   transports: [
-    transport,
+    fileRotateTransport,
+    new winston.transports.Console({
+      format: combine(colorize(), logFormat),
+    }),
   ],
 });
 
