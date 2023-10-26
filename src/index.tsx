725578cee85271c79e37d4fdf825ea4f6e5879a7
Dipsy Wong <ycwongal@connect.ust.hk>
2023-10-26 19:23:13 +0800

    fix test

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import { createRoot } from 'react-dom/client';
 
 import React from 'react';
 import App from './client/App';
-import reportWebVitals from './reportWebVitals';
 
 const container = document.getElementById('root');
 if (container !== null) {
@@ -13,8 +12,3 @@ if (container !== null) {
     </React.StrictMode>,
   );
 }
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
